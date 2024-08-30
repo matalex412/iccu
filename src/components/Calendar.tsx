6f4a65978c81097007c12d4d68e7d00c91fb4bf7
Matthew Alex <matalex412@gmail.com>
2024-08-30 22:49:23 +0100

    feat: add firestore database for calendar events and about member cards

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -6,25 +6,43 @@ import "../styles/calendar.css";
 
 const localizer = momentLocalizer(moment)
 
-const events = [
-  {
-    start: new Date(),
-    end: new Date(),
-    title: "Some title",
-    allDay: true,
-  }
-]
+// const events = [
+//   {
+//     start: new Date(),
+//     end: new Date(),
+//     title: "Some title",
+//     allDay: true,
+//   }
+// ]
 
-export default function MyCalendar () {
+export default function MyCalendar ({ events }: { events: any[] }) {  
+  var date = new Date();
+  date.setDate(date.getDate() - 1);
+
+  const fakeEvents = [
+    {
+      start: date,
+      end: date,
+      title: "Sometitle",
+      allDay: true,
+    }
+  ]
+
+  console.log('events', events)
+  console.log('fakeEvents', fakeEvents)
+  
+  const isMobile = false
+  const view = isMobile ? 'week' : 'month'
   return (
-      <Calendar
-        localizer={localizer}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500 }}
-        className="w-full"
-        views={['month']}
-        events={events}
-      />
+    <Calendar
+      localizer={localizer}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+      className="w-full"
+      views={[view]}
+      view={view}
+      events={events}
+    />
   )
 }
