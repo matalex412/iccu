2237c28006090f23a2ca9266ec2aa6b545e8fbdb
Matthew Alex <matalex412@gmail.com>
2024-09-07 15:18:40 +0100

    refactor: convert sign in form to react component

--- src/components/AddEventForm.tsx
+++ src/components/AddEventForm.tsx
@@ -55,9 +55,9 @@ const AddEventForm = () => {
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-            clip-rule="evenodd"
+            clipRule="evenodd"
           ></path>
         </svg>
         Add Event
---
+++ src/components/SignInForm.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import FormInput from "./FormInput";
+import { getAuth, inMemoryPersistence, signInWithEmailAndPassword } from "firebase/auth";
+import { app } from "../firebase/client";
+
+const auth = getAuth(app);
+auth.setPersistence(inMemoryPersistence);
+
+const SignInForm = () => {
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const email = formData.get("email")?.toString();
+    const password = formData.get("password")?.toString();
+
+    if (!email || !password) {
+      return;
+    }
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const idToken = await userCredential.user.getIdToken();
+    const response = await fetch("/api/auth/signin", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${idToken}`,
+      },
+    });
+
+    if (response.redirected) {
+      window.location.assign(response.url);
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit} className="p-5 flex flex-col gap-5">
+      <FormInput
+        label="Email"
+        inputProps={{
+          type: "email",
+          name: "email",
+          id: "email",
+          placeholder: "Enter email here",
+          required: true,
+        }}
+      />
+      <FormInput
+        label="Password"
+        inputProps={{
+          type: "password",
+          name: "password",
+          id: "password",
+          placeholder: "Enter password here",
+          required: true,
+        }}
+      />
+      <button type="submit" className="bg-leaf-300 text-white p-2 rounded">
+        Sign In
+      </button>
+    </form>
+  );
+};
+
+export default SignInForm;
